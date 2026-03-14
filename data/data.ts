a9fa034ee923e263a61b4e6b568e1044cd591096
libaidebai <carloswhitea1e@gmail.com>
2026-03-14 17:51:34 +0800

    chore: 优化数据结构

--- data/data.ts
+++ data/data.ts
@@ -9,56 +9,20 @@ import nissan from '@/data/Japan/nissan';
 import mg from '@/data/UK/mg';
 import peugeot from '@/data/France/peugeot';
 
-const 现代 = hyundai;
-const KIA = kia;
-const 大众 = volkswagen;
-const 标致 = peugeot;
-const 荣威 = roewe;
-const 丰田 = toyota;
-const 本田 = honda;
-const 马自达 = mazda;
-const 名爵 = mg;
-const 尼桑 = nissan;
+const brandMap = {
+  '现代 Hyundai': hyundai,
+  '起亚 KIA': kia,
+  '大众 VolksWagen': volkswagen,
+  '标致 Peugeot': peugeot,
+  '荣威 Roewe': roewe,
+  '名爵 MG': mg,
+  '丰田 Toyota': toyota,
+  '本田 Honda': honda,
+  '马自达 Mazda': mazda,
+  '日产尼桑 Nissan': nissan,
+};
 
-export const brands: Brand[] = [
-  {
-    name: '现代 Hyundai',
-    data: 现代
-  },
-  {
-    name: '起亚 KIA',
-    data: KIA
-  },
-  {
-    name: '大众 VolksWagen',
-    data: 大众
-  },
-  {
-    name: '标致 Peugeot',
-    data: 标致
-  },
-  {
-    name: '荣威 Roewe',
-    data: 荣威
-  },
-  {
-    name: '名爵 MG',
-    data: 名爵
-  },
-  {
-    name: '丰田 Toyota',
-    data: 丰田
-  },
-  {
-    name: '本田 Honda',
-    data: 本田
-  },
-  {
-    name: '马自达 Mazda',
-    data: 马自达
-  },
-  {
-    name: '日产尼桑 Nissan',
-    data: 尼桑
-  },
-]
+export const brands: Brand[] = Object.entries(brandMap).map(([name, data]) => ({
+  name,
+  data,
+}));
